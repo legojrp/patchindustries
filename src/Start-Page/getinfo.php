22a2fb0f6306ac2954eeaa07b217192c2022e19a
legojrp <84683257+legojrp@users.noreply.github.com>
2023-01-28 13:36:17 -0500

    still not finished with custom start page

--- src/Start-Page/getinfo.php
+++ src/Start-Page/getinfo.php
@@ -1,6 +1,23 @@
 <?php 
 
-$data = $_GET["data"];
-echo $data;
+    $id = $_GET["id"];
+    $file = fopen("./data/" . $id . ".data" , "r") 
+    $json = fread($file, filesize("./data/" . $id . ".data"));
+    fclose($file        );
+    $jsondecode = json_decode($json);
+    if ($jsondecode){
+        echo $json;
+    }
+    else {
+        $data->error = 200;
 
+    }
+
+
+
+
+    //Error code
+    // 1__ : soft error
+    // 2__ : hard read error
+    
 ?>
