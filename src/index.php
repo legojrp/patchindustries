e336f65728a8da78e6d0b85c62bb2c19596dc4bd
legojrp <legojrp@gmail.com>
2023-10-09 18:40:30 -0400

    main page again to fix what i messed up before

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <p>
       <?php echo json_decode($_GET["data"]) ?>
 </p>
-<p>This is patchindustries.com. I don't know whats here at the moment, because everything i make could be in a subdirectory. This is a playground testing website. With that, turtle game is <a href="/turtle/"></a> or my work in progress, not even close to in progress <a href="/Custom-Start-Page/">here</a>. Heres github for you <a href="https://github.com/legojrp"></a> </p>
+<p>This is patchindustries.com. I don't know whats here at the moment, because everything i make could be in a subdirectory. This is a playground testing website. With that, turtle game is <a href="/turtle/">here</a> or my work in progress, not even close to in progress <a href="/Custom-Start-Page/">here</a>. Heres github for you <a href="https://github.com/legojrp">here</a>. </p>
   
 <script>
   var json = {
