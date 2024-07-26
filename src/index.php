0f787879fbfb4bb356df1c20e28c944422799e47
legojrp <legojrp@gmail.com>
2024-07-26 16:27:01 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <body>
-<p>This is patchindustries.com. I don't know whats here at the moment, because everything i make could be in a subdirectory. This is a playground testing website. With that, turtle game is <a href="/turtle/">here</a> or my work in progress, not even close to in progress <a href="/Custom-Start-Page/">here</a>. Heres github for you <a href="https://github.com/legojrp">here</a>. </p>
+<p>This is patchindustries.com. Spoons card game is here <a href="/spoons/client/"></a> This is a playground testing website. Turtle game is <a href="/turtle/">here</a> (It takes a sec to load). Heres github for you <a href="https://github.com/legojrp">here</a>. </p>
   
 </body>
 </html>
