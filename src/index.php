8a8995ee69482748505d04fb33dbb38bb57da55b
legojrp <legojrp@gmail.com>
2024-07-26 16:27:45 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <body>
-<p>This is patchindustries.com. Spoons card game is here <a href="/spoons/client/"></a> This is a playground testing website. Turtle game is <a href="/turtle/">here</a> (It takes a sec to load). Heres github for you <a href="https://github.com/legojrp">here</a>. </p>
+<p>This is patchindustries.com. Spoons card game is <a href="/spoons/client/">here</a> This is a playground testing website. Turtle game is <a href="/turtle/">here</a> (It takes a sec to load). Heres github for you <a href="https://github.com/legojrp">here</a>. </p>
   
 </body>
 </html>
